51cb6eeb462d06f8426de450a8514de6abd2b14d
Paul Soh <ypsoh@orthus.nic.uoregon.edu>
2020-08-04 21:00:51 -0700

    Add additional flags to training

--- eval_wmt17_en_de.sh
+++ eval_wmt17_en_de.sh
@@ -4,6 +4,6 @@ source ~/.bashrc
 conda activate fairseq
 
 fairseq-generate data-bin/wmt17_en_de \
-      --path checkpoints/wmt17_en_de/checkpoint_best.pt \
-      --batch-size 128 --beam 5 --remove-bpe | tee /tmp/gen.out
+      --path checkpoints/checkpoint_best.pt \
+      --batch-size 128 --beam 5 --remove-bpe #| tee /tmp/gen.out
 
