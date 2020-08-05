7b413f91e513cfa84f0bc83c08a7e5f968e2d35e
Paul Soh <ypsoh@orthus.nic.uoregon.edu>
2020-08-04 21:09:57 -0700

    Add scripts for orthus systems

--- launch_pruning_runs.sh
+++ launch_pruning_runs.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
-target_sparsity=(0.95 0.8 0.5)
-prune_type=('random' 'magnitude')
+target_sparsity=(0.0 0.95 0.8)
+prune_type=('magnitude')
 
 for PRUNE_TYPE in ${prune_type[*]}; do
 for TARGET_SPARSITY in ${target_sparsity[*]}; do
-    sbatch -o ${PRUNE_TYPE}_${TARGET_SPARSITY}.out ./prune_en_de.sh ${TARGET_SPARSITY} ${PRUNE_TYPE}
+    #sbatch -o ${PRUNE_TYPE}_${TARGET_SPARSITY}.out ./prune_en_de.sh ${TARGET_SPARSITY} ${PRUNE_TYPE}
+    CUDA_VISIBLE_DEVICES=0,1,2,3 ./prune_en_de.sh ${TARGET_SPARSITY} ${PRUNE_TYPE} 1
 done;
 done;
